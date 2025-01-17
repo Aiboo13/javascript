65c4b0836d600aa10e74c3770a29d1f9e83eb7fa
MINASSAHIDDIN <minassahiddin@gmail.com>
2025-01-17 22:59:06 +0700

    switch case

--- script.js
+++ script.js
@@ -1,23 +1,47 @@
-console.log('haiii');
+// console.log('haiii');
 
-const Nilai = '60';
+// const Nilai = '60';
 
-if (Nilai >= 90) {
-  console.log('A');
-} else if(Nilai >= 70) {
-  console.log('B')
-} else if(Nilai >= 65) {
-  console.log('C')
-} else if(Nilai >= 50) {
-  console.log('D')
-}
+// if (Nilai >= 90) {
+//   console.log('A');
+// } else if(Nilai >= 70) {
+//   console.log('B')
+// } else if(Nilai >= 65) {
+//   console.log('C')
+// } else if(Nilai >= 50) {
+//   console.log('D')
+// }
 
 
-const Ganjilgenap = prompt('isikan angaka');
+// const Ganjilgenap = prompt('isikan angaka');
 // prompt mengambil inputan
 
-if (Ganjilgenap % 2 == 0) {
-  console.log('number your input is genap');
-} else {
-  console.log('number your input is ganjil');
+// if (Ganjilgenap % 2 == 0) {
+//   console.log('number your input is genap');
+// } else {
+//   console.log('number your input is ganjil');
+// }
+
+// 
+
+console.log('pick you eat with code');
+console.log('1 = mie');
+console.log('2 = ayam');
+console.log('3 = bakso');
+
+const masukanKode = prompt('masukan code');
+
+switch(masukanKode){
+  case '1':
+  console.log('anda memilih mie');
+  break;
+  case '2':
+  console.log('anda memilih ayam');
+  break;
+  case '3':
+  console.log('anda memilih bakso');
+  break;
+  default:
+    console.log('code yang anda masukan tidak ada');
 }
+
