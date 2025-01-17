969005832cc9fd34c59806887644f1ce0b7396ff
MINASSAHIDDIN <minassahiddin@gmail.com>
2025-01-17 21:27:28 +0700

    if stanmen

--- script.js
+++ script.js
@@ -5,4 +5,14 @@ const Namabelakang =" sahiddin";
 
 const Namalengkap = Namadepan + Namabelakang;
 
-alert(Namalengkap);
+// alert(Namalengkap);
+
+let angka = Math.random();
+console.log(angka);
+
+if(angka <= 0.5){
+  console.log("angka lebih kecil dari 0.5");
+}
+if(angka >= 0.5){
+  console.log("angka lebih besar dari 0.5");
+}
