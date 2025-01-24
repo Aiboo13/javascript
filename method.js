c1811abe2bd356f3e1c5bcc0081dc8a6df6840e7
MINASSAHIDDIN <minassahiddin@gmail.com>
2025-01-24 22:25:32 +0700

    implisit return

--- method.js
+++ method.js
@@ -1,8 +1,18 @@
 // 
 
-console.log("this is part arrow function")
+console.log("this is part arrow function");
 
 const perpangkatan = (x) => {
   // jadi '=>' adalah tanda wajib
   return x * x;
-}
+};
+
+
+console.log('implisin return atau tidak menggunakan return dama function jika perintahnya dalam function pendek atau hanya satu baris');
+
+const random = () =>(
+  Math.floor(Math.random() * 89) + 1
+);
+
+
+
