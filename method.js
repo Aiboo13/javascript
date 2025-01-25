e86b0257f5a0b59077071b92114972b9295e63fa
MINASSAHIDDIN <minassahiddin@gmail.com>
2025-01-25 15:36:03 +0700

    filter and  mapping

--- method.js
+++ method.js
@@ -16,23 +16,71 @@
 
 // console.log(random());
 
+let films =[
+  {
+    title : 'jujutsu kaisen',
+    rate : 90,
+  },
+  {
+    title : 'dotor strange',
+    rate : 84,
+  },
+  {
+    title : 'spiderman-noway-home',
+    rate : 83,
+  },
+  {
+    title : 'home alone',
+    rate : 94,
+  },
+  {
+    title : 'home alone 2',
+    rate : 78,
+  },
+  {
+    title : 'kimetsu no yoiba',
+    rate : 94,
+  },
+  {
+    title : 'transformers',
+    rate : 74,
+  }
+];
 
-console.log("time out and time interval");
+console.log("this part for 'map'")
 
-setTimeout(() =>{
-  console.log("jika pesan ini muncul maka kode di esekusi setelah 3 detik")
-}, 3000);
+let mapping = films.map((upper) => {
+  return upper.title.toUpperCase();
+})
+
+let ratting = films.filter((n) => {
+  return n.rate >= 90;
+})
+
+console.log("combination filter and map");
+
+let Filmbagus = films.filter((film) => film.rate >= 80 ).map((judul) =>{
+  console.log(`${judul.title} dengan rate ${judul.rate}`)
+} )
+
+
+
+// console.log("time out and time interval");
+
+// setTimeout(() =>{
+//   console.log("jika pesan ini muncul maka kode di esekusi setelah 3 detik")
+// }, 3000);
 
 
 
 function loop(){
   for(let i= 0; i <= 10000; i++){
     setTimeout(() => {
-      console.log(i);
+      console.log(`haloooooooooooooooooooooooooooooooo ${i}`);
     }, i * 500);
   }
 }
 
-loop();
+// loop();
 
 
