893daf6160cff1770e02bd0dfefcae0d548e61e1
MINASSAHIDDIN <minassahiddin@gmail.com>
2025-01-28 20:57:26 +0700

    method dalam distruck ga peduli linya dimana selama masi dalam satu scope

--- method.js
+++ method.js
@@ -396,6 +396,7 @@ class Color {
     this.g = g;
     this.b = b;
     this.name = name;
+    this.calcHSL();
     }
     Colorname(){
       console.log("this is name color " + this.name)
@@ -412,24 +413,87 @@ class Color {
       return `rgb(${this.innerRgb()})`;
       // saat memangil method harus menggunakan kurang buka dan tutup()
     }
+    // membuat method
     rgba(a=1.0){
       const {r,g,b} = this;
       // this ini merujuk pada argument constructor
       return `rgba(${r},${g},${b},${a})`;
     }
+    // membuat method
     hex(){
       const {r,g,b} = this;
       // this ini merujuk pada argument constructor
       return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
     }
+
+    hsl(){
+      const {h,s,l} = this;
+      return `hsl(${h}, ${s}%, ${l}%)`
+    }
+
+    fulllysaturated(){
+      const {h,l} = this;
+      return `hsl(${h}, 100%, ${l}%)`
+    }
+
+    opposite(){
+      const {h,s,l} = this;
+      const newHue = (h + 180) % 360;
+      return `hsl(${newHue}, ${s}%, ${l}%)`;
+    }
+    
+    calcHSL(){
+      let { r, g, b } = this;
+      // Make r, g, and b fractions of 1
+      r /= 255;
+      g /= 255;
+      b /= 255;
+  
+      // Find greatest and smallest channel values
+      let cmin = Math.min(r, g, b),
+        cmax = Math.max(r, g, b),
+        delta = cmax - cmin,
+        h = 0,
+        s = 0,
+        l = 0;
+      if (delta == 0) h = 0;
+      else if (cmax == r)
+        // Red is max
+        h = ((g - b) / delta) % 6;
+      else if (cmax == g)
+        // Green is max
+        h = (b - r) / delta + 2;
+      // Blue is max
+      else h = (r - g) / delta + 4;
+  
+      h = Math.round(h * 60);
+  
+      // Make negative hues positive behind 360°
+      if (h < 0) h += 360;
+      // Calculate lightness
+      l = (cmax + cmin) / 2;
+  
+      // Calculate saturation
+      s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
+  
+      // Multiply l and s by 100
+      s = +(s * 100).toFixed(1);
+      l = +(l * 100).toFixed(1);
+      this.h = h;
+      this.s = s;
+      this.l = l;
+    }
+  
+  
 }
 
-const seaColor = new Color(152, 216, 239,"sea")
-// jangan lupa kata kunci new 
-document.body.style.backgroundColor = seaColor.rgba()
-console.log(seaColor.name);
-console.log(seaColor.innerRgb());
+const seaColor = new Color(152, 216, 239,"sea");
+// // jangan lupa kata kunci new 
+// document.body.style.backgroundColor = seaColor.rgba()
+// console.log(seaColor.name);
+// console.log(seaColor.innerRgb());
 
 
 // kesimpulan jadi tanda .name method itu ungu maka itu menggunakan tanda kurung buka dan tutup
 
+console.log("hsl srgb")
